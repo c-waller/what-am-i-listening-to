a6da78254ea1b5a2babe1b5b386591332584148b
cwaller33 <59543955+cwaller33@users.noreply.github.com>
2025-02-24 23:11:25 -0500

    Made some changes to styling/content. Cool emoji animation too!

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './styles.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
 function App() 
 {
   return (
-    <div className="container">
-
-      <h1 id ="title"> What Music Am I <br /> Even Listening To? </h1>
-      <p id ="artists"> Come test your knowledge of your <br />personal taste in tunes... <br /></p>
-      <button id="quiz-button"> Let's Go! </button>
-      
-    </div>
+    <>
+      {/* <header></header> */}
+      <div className="container">
+        <h1 id ="title"> What Music Am I <br /> Even Listening To? </h1>
+        <p id ="artists"> Test your knowledge of your personal taste in <span id = "note"> 🎵 </span><br /></p>
+        {/* <p id ="artists"> Come test your knowledge of your <br />personal taste in  "The Hellp"... <br /></p> */}
+        <button id="quiz-button"> Are You Ready? </button>
+      </div>
+    </>
   )
 }
-
 export default App
