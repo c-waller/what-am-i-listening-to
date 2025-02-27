8562d9fc2859fc628baef4856d8c01e71cfda1a4
cwaller33 <59543955+cwaller33@users.noreply.github.com>
2025-02-27 15:42:46 -0500

    Set up authentication with Spotify

--- src/components/AuthenticationBox.tsx
+++ src/components/AuthenticationBox.tsx
@@ -1,5 +1,11 @@
+import { loginUrl } from "../pages/Authenticate";
+
 function AuthenticationBox()
 {
-  return <button id="auth-button"><img src="src/assets/images/spotify-logo.png" width={25}></img>Connect with Spotify</button>
+  return (
+        <a href={loginUrl}> 
+          <button id="auth-button"><img src="src/assets/images/spotify-logo.png" width={25}></img>Connect with Spotify</button>
+        </a>
+  )
 }
 export default AuthenticationBox;
--- src/pages/Authenticate.tsx
+++ src/pages/Authenticate.tsx
@@ -3,15 +3,28 @@ import AuthenticationBox from '../components/AuthenticationBox';
 import slideOut from '../transitions/slideOut';
 import '../styles/authentication.css';
 
+const scopes = [
+  "playlist-read-private",
+  "user-read-private", // account type
+  "user-read-recently-played",
+  "user-library-read", 
+  "user-follow-read",
+  "user-top-read"
+];
+
+const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:5173/quiz";
+const clientId = ""; // MOVE THIS BEFORE PUSHING LMFAO
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+
 function Authenticate()
 {
   const [showBox, setShowBox] = useState(false);
   useEffect(() => {setTimeout(() => {setShowBox(true)}, 3000)}, []); // setShowBox to true after 3 secs
-
   return (
     <div className="container-authentication">
       {/* Only show AuthenticatinBox when showBox is True (after 3 seconds)*/}
-      {showBox ? (<AuthenticationBox />) : (<p id="content">First let's take a look at your spotify...</p>)}
+      {showBox ? (<AuthenticationBox />) : (<p id="content"> First let's take a look at your spotify... </p>)}
     </div>
   );
 }
