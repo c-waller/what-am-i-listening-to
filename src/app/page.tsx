5e8f258964b5a7b74e63f0c36eb292704748b777
cwaller33 <59543955+cwaller33@users.noreply.github.com>
2025-03-05 23:54:41 -0500

    Minor styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home()
       </div>
       <div className={styles.containerLanding}>
         <h1 className={styles.title}> What Music Am I <br/> Even Listening To? </h1>
-        <p className={styles.content}> View your top songs, artists, and more! </p>
+        <p className={styles.content}> View your favorite tracks and artists with a click of a button! </p>
         <Link href="/auth">
           <MainButton> Let's Get Started </MainButton>
         </Link>
